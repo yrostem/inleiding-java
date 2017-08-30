3fd59a1ef93a2e4487bdf67f439fd5a8a0a59759
yrostem <yrostem@roc-dev.com>
2017-08-30 11:47:46 +0200

    Nieuwe text

--- src/h01/HalloRocDev.java
+++ src/h01/HalloRocDev.java
@@ -6,6 +6,6 @@ package h01;
 public class HalloRocDev {
 
     public static void main(String[] args) {
-        System.out.println("wijziging in git");
+        System.out.println("pushen");
     }
 }
