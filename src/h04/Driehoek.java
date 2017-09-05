f2be54a263e22731a3e189797c8296c06c2648b9
yrostem <yrostem@roc-dev.com>
2017-09-05 13:05:02 +0200

    Driehoek gemaakt

---
+++ src/h04/Driehoek.java
@@ -0,0 +1,15 @@
+package h04;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Driehoek extends Applet{
+
+    public void init() {
+    }
+
+    public void paint(Graphics g) {
+        setBackground(Color.blue);
+        g.setColor(Color.yellow);
+    }
+}
