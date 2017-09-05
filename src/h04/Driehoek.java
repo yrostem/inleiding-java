7a28faf718944d5d9a2a0152d927f7dfde014231
yrostem <yrostem@roc-dev.com>
2017-09-05 14:02:54 +0200

    Driehoek gemaakt

--- src/h04/Driehoek.java
+++ src/h04/Driehoek.java
@@ -2,21 +2,18 @@ package h04;
 
 import java.applet.Applet;
 import java.awt.*;
-import java.awt.MouseInfo;
-
 
 public class Driehoek extends Applet {
 
     public void init() {
+
     }
+
 
     public void paint(Graphics g) {
         setBackground(Color.blue);
         g.setColor(Color.yellow);
-        int xpoints[] = {5, 105, 25, 145, 25};
-        int ypoints[] = {25, 25, 145, 145, 25};
-        int npoints = 5;
-
-        g.drawPolygon(xpoints, ypoints, npoints);
+        g.drawLine(50, 50, 10, 40);
+        g.drawLine(50, 120, 270, 120);
     }
 }
