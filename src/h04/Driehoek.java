3ad8d474e1893a34d05c45f35f1cd813fc1c9515
yrostem <yrostem@roc-dev.com>
2017-09-07 12:58:04 +0200

    Driehoek gemaakt

--- src/h04/Driehoek.java
+++ src/h04/Driehoek.java
@@ -13,7 +13,11 @@ public class Driehoek extends Applet {
     public void paint(Graphics g) {
         setBackground(Color.blue);
         g.setColor(Color.yellow);
-        g.drawLine(50, 50, 10, 40);
-        g.drawLine(50, 120, 270, 120);
+        ///gelijkbenige driehoek
+        g.drawLine(100, 150, 200, 150); //Onderste lijn
+        g.drawLine(100, 150, 150, 50); //Linker lijn
+        g.drawLine(200, 150, 150, 50); //Rechter lijn
+        ///Einde gelijkbenige driehoek
+
     }
 }
