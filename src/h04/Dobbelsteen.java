431e3a326aed627402fdddfd2eb0d0c30291a230
yrostem <yrostem@roc-dev.com>
2017-09-08 09:43:24 +0200

    Dobbelsteen gemaakt

--- src/h04/Dobbelsteen.java
+++ src/h04/Dobbelsteen.java
@@ -9,7 +9,7 @@ public class Dobbelsteen extends Applet{
     }
     public void paint(Graphics g){
         g.drawRect(80,100,110,110);
-        g.setColor(Color.black);
+        g.setColor(Color.black);//kleur
         g.fillOval(100,120,20,20);
         g.fillOval(100,170,20,20);
         g.fillOval(150,170,20,20);
