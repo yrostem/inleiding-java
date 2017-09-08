b2852331f0b2dae867d4b22832d38b086428ce1a
yrostem <yrostem@roc-dev.com>
2017-09-08 09:42:43 +0200

    Stoplicht gemaakt

---
+++ src/h04/Dobbelsteen.java
@@ -0,0 +1,18 @@
+package h04;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Dobbelsteen extends Applet{
+    public void init(){
+
+    }
+    public void paint(Graphics g){
+        g.drawRect(80,100,110,110);
+        g.setColor(Color.black);
+        g.fillOval(100,120,20,20);
+        g.fillOval(100,170,20,20);
+        g.fillOval(150,170,20,20);
+        g.fillOval(150,120,20,20);
+    }
+}
