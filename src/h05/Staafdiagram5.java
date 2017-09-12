8e29ba8944a1bd87af8dd3be19c89ed5c86543a2
yrostem <yrostem@roc-dev.com>
2017-09-12 12:36:19 +0200

    Staafdiagram met variabel gemaakt

--- src/h05/Staafdiagram5.java
+++ src/h05/Staafdiagram5.java
@@ -9,9 +9,9 @@ public class Staafdiagram5 extends Applet {
     int nullijn;
     public void init(){
         nullijn = 250;
-        Valeriegewicht=40;
-        Hansgewicht=80;
-        Jeroengewicht=100;
+        Valeriegewicht=65;
+        Hansgewicht=30;
+        Jeroengewicht=52;
     }
     public void paint(Graphics g){
         g.drawLine(20,nullijn,20,50);//Gewicht lijn
@@ -22,13 +22,13 @@ public class Staafdiagram5 extends Applet {
         g.drawString("80", 5 , 100);
         g.drawString("100", 1 , 60);
         g.setColor(Color.RED);
-        g.fillRect(30, nullijn-40*2,30, Valeriegewicht*2 );//Rode diagram
+        g.fillRect(30, nullijn-Valeriegewicht*2,30, Valeriegewicht*2 );//Rode diagram
         g.drawString("Valerie", 30, 270);//Valerie
         g.setColor(Color.BLUE);
-        g.fillRect(80, nullijn-80*2, 30, Hansgewicht*2);
+        g.fillRect(80, nullijn-Hansgewicht*2, 30, Hansgewicht*2);
         g.drawString("Hans", 85, 270);
         g.setColor(Color.GREEN);
-        g.fillRect(130, nullijn-100*2, 30, Jeroengewicht*2);
+        g.fillRect(130, nullijn-Jeroengewicht*2, 30, Jeroengewicht*2);
         g.drawString("Jeroen", 130, 270);
     }
 }
