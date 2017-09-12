6676c4f8ff88ab7e13ae2214f03e2d62d3db5db2
yrostem <yrostem@roc-dev.com>
2017-09-12 13:19:55 +0200

    Seconden,uur,dag,jaar berekening gemaakt

---
+++ src/h06/Opdracht2.java
@@ -0,0 +1,20 @@
+package h06;
+import java.awt.*;
+import java.applet.*;
+
+public class Opdracht2 extends Applet {
+    int seconden,uur, dag, jaar;
+    double uitkomst;
+
+    public void init(){
+        seconden = 60;
+        uur = seconden*60;
+        dag = uur*24;
+        jaar = dag*365;
+    }
+    public void paint(Graphics g){
+        g.drawString("In een uur zitten: "+uur+" seconden",20,50);
+        g.drawString("In een dag (24 uur) zitten: "+dag+" seconden",20,80);
+        g.drawString("In een jaar (365 dgn) zitten: "+jaar+" seconden",20,110);
+    }
+}
